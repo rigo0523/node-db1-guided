5e0f5e54dece20b81cf119545545b2b02cd72593
Rigo <limon1285@gmail.com>
2020-11-14 00:07:40 -0600

    done

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
-const express = require("express")
-const welcomeRouter = require("./welcome/welcome-router")
-const messagesRouter = require("./messages/messages-router")
+const express = require("express");
+const welcomeRouter = require("./welcome/welcome-router");
+const messagesRouter = require("./messages/messages-router");
 
-const server = express()
-const port = process.env.PORT || 4000
+const server = express();
+const port = process.env.PORT || 4000;
 
-server.use(express.json())
-server.use("/", welcomeRouter)
-server.use("/messages", messagesRouter)
+server.use(express.json());
+server.use("/", welcomeRouter);
+server.use("/api/messages", messagesRouter);
 
+//middleware for CATCH ERROR on all endpoints of /api/messages
 server.use((err, req, res, next) => {
-	console.log(err)
-	res.status(500).json({
-		message: "Something went wrong",
-	})
-})
+  console.log(err);
+  res.status(500).json({
+    message: "500 error: Something went wrong",
+  });
+});
 
 server.listen(port, () => {
-	console.log(`Server running at http://localhost:${port}`)
-})
+  console.log(`Server running at http://localhost:${port}`);
+});
--- messages/messages-router.js
+++ messages/messages-router.js
@@ -1,26 +1,184 @@
-const express = require("express")
-const db = require("../data/config")
+const express = require("express");
+const db = require("../data/config");
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/", (req, res, next) => {
+//CRUD WITH JSON MAURER
 
-})
+//GET /api/messages
+router.get("/", async (req, res, next) => {
+  try {
+    //SELECT * FROM messages
+    const messages = await db.select("*").from("messages");
+    res.status(200).json({ data: messages });
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.get("/:id", (req, res, next) => {
+//GET BY ID /api/messages/:id
+router.get("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    //SELECT * FROM messages WHERE id = ? limit 1 by destruction message inside[""]
+    const [message] = await db
+      .select("*")
+      .from("messages")
+      .where({ id })
+      .limit(1);
+    res.json({ data: message });
+  } catch (err) {
+    next(err);
+  }
+});
 
-})
+//POST /api/messages
+router.post("/", async (req, res, next) => {
+  const newPost = req.body;
+  try {
+    //SELECT * FROM messages
+    // const message = await db
+    //   .select("*")
+    //   .from("messages")
+    //   //other databases require returning("id") or you can add "id"
+    //   //inside insert after newPost
+    //   .insert(newPost, "id")
+    //   .then((ids) => {
+    //     db("messages")
+    //       .where({ id: ids })
+    //       .first()
+    //       .then((post) => {
+    //         res.json({ data: post });
+    //       });
+    //   });
 
-router.post("/", (req, res, next) => {
+    ///Destructuring method instead, much shorter
+    const [id] = await db
+      .insert({ title: req.body.title, contents: req.body.contents })
+      .into("messages");
 
-})
+    const message = await db("messages").where({ id: id }).first();
+    res.status(201).json({ data: message });
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.put("/:id", (req, res, next) => {
+//UPDATE /api/messages/:id
+router.put("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  const title = req.body;
+  const contents = req.body;
+  try {
+    //SELECT * FROM messages
 
-})
+    let messages = await db("messages")
+      .update(title, contents)
+      .where({ id: id })
+      .then((ids) => {
+        db("messages")
+          .where({ id: ids })
+          .first()
+          .then((changes) => {
+            res.status(200).json({ newUPDATE: changes });
+          });
+      });
+  } catch (err) {
+    next(err);
+  }
+});
 
-router.delete("/:id", (req, res, next) => {
+//DELETE /api/messages/:id
+router.delete("/:id", async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    //SELECT * FROM messages
+    let messages = await db("messages").where({ id }).del();
 
-})
+    res.status(204).json({ data: messages });
+  } catch (err) {
+    next(err);
+  }
+});
 
-module.exports = router
+module.exports = router;
+
+// //CRUD with Luis Hernandez without ASYNC
+// router.get("/", (req, res) => {
+//   db("messages")
+//     .then((message) => {
+//       res.json(message);
+//     })
+//     .catch((err) => {
+//       res.json({ message: "error" });
+//     });
+// });
+
+// router.get("/:id", (req, res) => {
+//   const { id } = req.params;
+//   db("messages")
+//     .where({ id: id })
+
+//     .then((result) => {
+//       res.json({ data: result });
+//     })
+//     .catch((erro) => {
+//       res.json({ message: " can't find by ID" });
+//     });
+// });
+
+// router.post("/", (req, res) => {
+//   const newPost = req.body;
+//   db("messages")
+//     // OR .insert(newPost).returning("id").then().catch(); <<<works too
+//     //Whenever you insert always pass a second argument
+//     .insert(newPost, "id")
+//     .then((ids) => {
+//       db("messages")
+//         .where({ id: ids })
+//         .then((post) => {
+//           res.json({ data: post });
+//         })
+//         .catch((err) => res.json({ message: "cant post" }));
+//     })
+//     .catch((err) => {
+//       res.json(err);
+//     });
+// });
+
+// router.put("/:id", (req, res, next) => {
+//   const change = req.body;
+//   const { id } = req.params;
+//   db("messages")
+//     .update(change)
+//     .where({ id: id })
+//     .then((ids) => {
+//       db("messages")
+//         .where({ id: ids })
+//         .then((changes) => {
+//           res.json({ data: changes });
+//         })
+//         .catch((err) => res.json({ message: "can't update ID ", id }));
+//     })
+//     .catch((err) => {
+//       res.json({ message: "error posting" });
+//     });
+// });
+
+// router.delete("/:id", (req, res, next) => {
+//   const { id } = req.params;
+//   db("messages")
+//     .where({ id: id })
+//     .del()
+//     .then((deleted) => {
+//       if (deleted > 0) {
+//         res.json({ message: `Id of ${id} deleted`, data: deleted });
+//       } else {
+//         res.json({ message: `can't deleted by that id of ${id}` });
+//       }
+//     })
+
+//     .catch((err) => res.json({ message: `ID of ${id} NOT FOUND` }));
+// });
+
+// module.exports = router
